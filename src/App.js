582ef7a45b1db789be3efb80da18f700e4b99b2e
Justinnkar <jkarpman01@gmail.com>
2023-05-08 11:56:56 -0700

    goals completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
-import "./App.css"
-import Square from "./components/Square"
+import React, { useState } from "react";
+import "./App.css";
+import Square from "./components/Square";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -13,31 +13,62 @@ const App = () => {
     "?",
     "?",
     "?"
-  ])
-  
-  const winningSquareIndex = 3
-  const losingSquareIndex = 6
+  ]);
+  const [remainingGuesses, setRemainingGuesses] = useState(5);
+  const [gameOver, setGameOver] = useState(false);
+  const winningSquareIndex = Math.floor(Math.random() * board.length);
+  const losingSquareIndex = Math.floor(Math.random() * board.length);
 
   const handleClick = (selectedIndex) => {
-    const newBoard = [...board]
+    if (gameOver) return; // game is over
+    const newBoard = [...board];
+    let updatedGuesses = remainingGuesses;
+
     if (selectedIndex === winningSquareIndex) {
-      newBoard[selectedIndex] = "💰" // update to treasure emoji
+      newBoard[selectedIndex] = "💰";
+      setGameOver(true);
+      alert("You won the game!"); // display message to user
     } else if (selectedIndex === losingSquareIndex) {
-      newBoard[selectedIndex] = "💣" // update to bomb emoji
+      newBoard[selectedIndex] = "💣";
+      setGameOver(true);
+      alert("You lost the game!"); // display message to user
     } else {
-      newBoard[selectedIndex] = "🌳" // update to tree emoji
+      newBoard[selectedIndex] = "🌳";
+      updatedGuesses -= 1;
+      if (updatedGuesses === 0) {
+        setGameOver(true);
+        alert("You lost the game!"); // display message to user
+      }
     }
-    setBoard(newBoard)
-  }
-  
+
+    setBoard(newBoard);
+    setRemainingGuesses(updatedGuesses);
+  };
+
+  const resetGame = () => {
+    setBoard([
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?"
+    ]);
+    setRemainingGuesses(5);
+    setGameOver(false);
+  };
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
-      <Square board={board}
-      handleClick={handleClick}/>
+      <Square board={board} handleClick={handleClick} />
+      {gameOver && <button onClick={resetGame}>Play Again</button>}
+      <p>Remaining Guesses: {remainingGuesses}</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,18 +1,19 @@
-import React from "react"
+import React from "react";
 
 const Square = (props) => {
   return (   
-      <div id="grid">
-        {props.board.map((value, index)=> {
-          return (
-            <div 
-              className="square" key={index}
-              onClick={() => {props.handleClick(index)}}
-            >{value}
-            </div>
-          )
-        })} 
-      </div>             
+    <div id="grid">
+      {props.board.map((value, index)=> {
+        return (
+          <div 
+            className="square" key={index}
+            onClick={() => {props.handleClick(index)}}
+          >{value}
+          </div>
+        )
+      })} 
+    </div>             
   )
-}
-export default Square
+};
+
+export default Square;
